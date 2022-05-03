a56d0b4617004a1cfc0a69f4131f93e10a0d2b26
Hmelo2000 <63059048+Hmelo2000@users.noreply.github.com>
2022-05-03 01:05:40 +0100

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -35,6 +35,39 @@ ridge_regression_model.fit(X, y)
 
 #Display the results (coefficients of the model)
 df_ratings = pd.DataFrame(data={'team': X.columns, 'rating': ridge_regression_model.coef_})
-print(df_ratings)
+#print(df_ratings)
+
+# Graphical User Interface
+
+#Importing Libraries
+from tkinter import *
+from PIL import ImageTk, Image
+ 
+root = Tk()
+root.title("American Sports Prediction System") #Set Title of the Window
+root.geometry("725x500") # Setting Dimensions
+
+Title = Label(root, text="Which sport (out of the 2 below) would you like to make a prediciton in: ", font=("Gamerock", 17, "bold"))
+Title.grid(row=0, column=0)
+
+#create button for the basketball sport
+BasketballButton = Button(root, text="Basketball", padx=4, pady=3, command=basketballfunction, fg="orange", font=("Gamerock", 14, "italic"))
+BasketballButton.grid(row=1, column=0) # Position the button
+
+#create button for the hockey sport
+HockeyButton = Button(root, text="Hockey", padx=4, pady=3, command=hockeyfunction, fg="blue", font=("Gamerock", 14, "italic"))
+HockeyButton.grid(row=2, column=0) # Position the button
+
+#Create and display downloaded sports image on the GUI 
+my_img = ImageTk.PhotoImage(Image.open("icon.jpeg"))
+photo = Label(image=my_img)
+photo.grid(row=3, column=0)
+
+#Create button for the prediction
+PredictButton = Button(root, text="Predict", padx=4, pady=4, command=predictfunction, fg="green")
+#Create button that will allow you to make other predictions
+AnotherPredictionButton = Button(root, text="Make Another Prediction", padx=4, pady=4, command=AnotherPrediction, fg="red")
+
+root.mainloop()
 
 
